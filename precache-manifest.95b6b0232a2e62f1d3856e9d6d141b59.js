3cbe82bcd24029ec1ec5d69560f6fa4abc5e5a76
smriti29 <smritip29@gmail.com>
2020-06-10 00:43:46 +0530

    Updates

---
+++ precache-manifest.95b6b0232a2e62f1d3856e9d6d141b59.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "554e3f674391f6276f83",
+    "url": "/authorquiz/static/js/runtime~main.9169a697.js"
+  },
+  {
+    "revision": "ce6ad503d1cf435ee9b2",
+    "url": "/authorquiz/static/js/main.581ac586.chunk.js"
+  },
+  {
+    "revision": "0796282488012de28b4a",
+    "url": "/authorquiz/static/js/2.39b06ef1.chunk.js"
+  },
+  {
+    "revision": "ce6ad503d1cf435ee9b2",
+    "url": "/authorquiz/static/css/main.0dd26df1.chunk.css"
+  },
+  {
+    "revision": "2460149e038d5bec76f3b3ec0070f3d4",
+    "url": "/authorquiz/index.html"
+  }
+];
